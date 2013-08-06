293d18deab6ef0772291e2618150e95e0a137712
Robert Conde <robert.conde@sungard.com>
2013-08-06 14:37:16 -0400

    Starting to work on ATS with pure Python

---
+++ atsModule.py
@@ -0,0 +1,32 @@
+"""Demo Rabbit MQ functionality."""
+import pika
+import FLogger
+
+logger = FLogger.FLogger(__name__,level=2)
+
+def start():
+    logger.DLOG('DEBUG LOGGING ENABLED')
+    global connection
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost')
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_consume(callback,queue='hello',no_ack=True)
+    logger.LOG('Waiting for messages...')
+    channel.start_consuming()
+
+    
+def stop():
+    print 'stop'*20
+    return
+    
+def status():
+    print 'status'*20
+    return
+    
+def work():
+    return
+
+def callback(ch,method,properties,body):
+    logger.DLOG('Received %r %r %r %r',ch,method,properties,body)
